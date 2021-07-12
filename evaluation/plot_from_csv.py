8ce29ccf8a06b421126668f36ef1e126e8ec8e05
Vivokas20 <vivianabernardo@tecnico.ulisboa.pt>
2021-07-12 23:10:31 +0100

    Update plot_from_csv.py

--- evaluation/plot_from_csv.py
+++ evaluation/plot_from_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
+from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
-
 
 ####################### Plot Functions ########################
 
@@ -10,15 +10,16 @@ def time_plot(datas, names):
 
     for n in range(len(datas)):
         df = datas[n]
-        # df = df[df.timeout == False]
+        df = df[df.timeout == False]
         df = df.sort_values("real").reset_index(drop=True)
         df.index += 1
         df = df.reset_index()
 
-        fig = df.plot(label= names[n], xlabel="Instances", ylabel="Real Time (s)", x="index", y="real", subplots=False, ax=ax)
+        fig = df.plot(label= names[n], xlabel="Instances", ylabel="Real Time (s)", x="index", y="real", style='.-', subplots=False, ax=ax)
+        # df.plot(style='.-', markevery=5)
 
     fig = fig.get_figure()
-    fig.savefig("evaluation/plots/time_plot.pdf")
+    return fig
 
 def programs_plot(datas, names):
     fig, ax = plt.subplots()
@@ -30,11 +31,12 @@ def programs_plot(datas, names):
         df.index += 1
         df = df.reset_index()
 
-        fig = df.plot(label= names[n], xlabel="Instances solved", ylabel="Attempted programs", x="index", y="programs", subplots=False, ax=ax)
+        fig = df.plot(label= names[n], xlabel="Instances solved", ylabel="Attempted programs", x="index", y="programs", style='.-', subplots=False, ax=ax)
+        # fig.yaxis.set_major_formatter(mtick.PercentFormatter())
         # fig = df.plot(label=names[n], xlabel="Instance", ylabel="Attempted programs", x="name", y="programs", subplots=False, ax=ax)
 
     fig = fig.get_figure()
-    fig.savefig("evaluation/plots/programs_plot.pdf")
+    return fig
 
 def solved_plot(datas, names):
     index = []
@@ -52,10 +54,28 @@ def solved_plot(datas, names):
     fig = df.plot(kind="barh", xlabel="Benchmark", ylabel="Solved instances") # figsize = (6.4, 4.8)
     fig.get_legend().remove()
     fig = fig.get_figure()
-    fig.savefig("evaluation/plots/solved_plot.pdf")
+    return fig
+
+def gt_plot(datas, names):
+    index = []
+    values = []
+
+    for n in range(len(datas)):
+        data = datas[n]
+        df = data[data.gt == True]
+        index.append(names[n])
+        values.append(len(df.index))
+
+    df = pd.DataFrame({"solved": values}, index=index)
+
+    rcParams.update({'figure.autolayout': True})
+    fig = df.plot(kind="barh", xlabel="Benchmark", ylabel="Solved instances") # figsize = (6.4, 4.8)
+    fig.get_legend().remove()
+    fig = fig.get_figure()
+    return fig
 
 
-files = ["evaluation/data/textbook-no_sketch.csv", "evaluation/data/textbook-no_children.csv", "evaluation/data/textbook-no_root.csv"]
+files = ["evaluation/data/textbook-no_sketch.csv", "evaluation/data/on/off_no_children.csv", "evaluation/data/on/on_no_children.csv", "evaluation/data/on/off_no_children_all_constraints.csv", "evaluation/data/on/off_no_children_constraints.csv"]
 csv_list = []
 name_list = []
 
@@ -63,7 +83,10 @@ for file in files:
     csv_list.append(pd.read_csv(file))
     name_list.append(file.rsplit("/", 1)[1][:-4])
 
-time_plot(csv_list, name_list)
-programs_plot(csv_list, name_list)
-solved_plot(csv_list, name_list)
+figs = [time_plot(csv_list, name_list), programs_plot(csv_list, name_list), solved_plot(csv_list, name_list), gt_plot(csv_list, name_list)]
+
+with PdfPages("evaluation/plots/plots.pdf") as pdf:
+    for fig in figs:
+        pdf.savefig(fig)
+
 # programs_plot([csv_list[1]], [name_list[1]])
