0200319f61a38a358a3ea14d3f38056ba5a3da1a
Vivokas20 <vivianabernardo@tecnico.ulisboa.pt>
2021-07-22 12:40:47 +0100

    Evaluation update

--- evaluation/plot_from_csv.py
+++ evaluation/plot_from_csv.py
@@ -3,6 +3,23 @@ from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
 
+flag_filter = False
+flag_summarise = False
+csv_list = []
+name_list = []
+
+filter = ['tests-examples/textbook/1', 'tests-examples/textbook/10', 'tests-examples/textbook/14', 'tests-examples/textbook/15', 'tests-examples/textbook/16', 'tests-examples/textbook/17', 'tests-examples/textbook/19', 'tests-examples/textbook/2', 'tests-examples/textbook/20', 'tests-examples/textbook/21', 'tests-examples/textbook/22', 'tests-examples/textbook/23', 'tests-examples/textbook/24', 'tests-examples/textbook/25', 'tests-examples/textbook/26', 'tests-examples/textbook/28', 'tests-examples/textbook/29', 'tests-examples/textbook/3', 'tests-examples/textbook/31', 'tests-examples/textbook/35', 'tests-examples/textbook/4', 'tests-examples/textbook/5', 'tests-examples/textbook/6', 'tests-examples/textbook/8', 'tests-examples/textbook/9', 'tests-examples/scythe/top_rated_posts/002', 'tests-examples/scythe/top_rated_posts/013', 'tests-examples/scythe/top_rated_posts/017', 'tests-examples/scythe/top_rated_posts/025', 'tests-examples/scythe/top_rated_posts/031', 'tests-examples/scythe/top_rated_posts/032', 'tests-examples/scythe/top_rated_posts/038', 'tests-examples/scythe/top_rated_posts/043', 'tests-examples/scythe/recent_posts/004', 'tests-examples/scythe/recent_posts/016', 'tests-examples/scythe/recent_posts/019', 'tests-examples/scythe/recent_posts/021', 'tests-examples/scythe/recent_posts/028', 'tests-examples/scythe/recent_posts/031', 'tests-examples/scythe/recent_posts/040', 'tests-examples/scythe/recent_posts/046', 'tests-examples/spider/architecture/0007', 'tests-examples/spider/architecture/0008', 'tests-examples/spider/architecture/0009', 'tests-examples/spider/architecture/0011', 'tests-examples/spider/architecture/0012', 'tests-examples/spider/architecture/0013', 'tests-examples/spider/architecture/0017']
+summarise = ['tests-examples/textbook/10', 'tests-examples/textbook/14', 'tests-examples/textbook/15', 'tests-examples/textbook/17', 'tests-examples/textbook/18', 'tests-examples/textbook/22', 'tests-examples/textbook/25', 'tests-examples/textbook/3', 'tests-examples/textbook/4', 'tests-examples/textbook/5', 'tests-examples/textbook/6', 'tests-examples/textbook/7', 'tests-examples/textbook/8', 'tests-examples/textbook/9', 'tests-examples/scythe/top_rated_posts/001', 'tests-examples/scythe/top_rated_posts/002', 'tests-examples/scythe/top_rated_posts/004', 'tests-examples/scythe/top_rated_posts/005', 'tests-examples/scythe/top_rated_posts/006', 'tests-examples/scythe/top_rated_posts/007', 'tests-examples/scythe/top_rated_posts/008', 'tests-examples/scythe/top_rated_posts/009', 'tests-examples/scythe/top_rated_posts/011', 'tests-examples/scythe/top_rated_posts/012', 'tests-examples/scythe/top_rated_posts/013', 'tests-examples/scythe/top_rated_posts/014', 'tests-examples/scythe/top_rated_posts/016', 'tests-examples/scythe/top_rated_posts/019', 'tests-examples/scythe/top_rated_posts/021', 'tests-examples/scythe/top_rated_posts/027', 'tests-examples/scythe/top_rated_posts/028', 'tests-examples/scythe/top_rated_posts/029', 'tests-examples/scythe/top_rated_posts/034', 'tests-examples/scythe/top_rated_posts/036', 'tests-examples/scythe/top_rated_posts/037', 'tests-examples/scythe/top_rated_posts/038', 'tests-examples/scythe/top_rated_posts/043', 'tests-examples/scythe/top_rated_posts/047', 'tests-examples/scythe/top_rated_posts/048', 'tests-examples/scythe/top_rated_posts/049', 'tests-examples/scythe/top_rated_posts/051', 'tests-examples/scythe/top_rated_posts/055', 'tests-examples/scythe/top_rated_posts/057', 'tests-examples/scythe/recent_posts/007', 'tests-examples/scythe/recent_posts/009', 'tests-examples/scythe/recent_posts/011', 'tests-examples/scythe/recent_posts/012', 'tests-examples/scythe/recent_posts/016', 'tests-examples/scythe/recent_posts/032', 'tests-examples/scythe/recent_posts/040', 'tests-examples/scythe/recent_posts/045', 'tests-examples/scythe/recent_posts/051', 'tests-examples/spider/architecture/0003', 'tests-examples/spider/architecture/0009', 'tests-examples/spider/architecture/0011']
+
+
+def greater_than(datas):
+    big = datas[0]
+    small = datas[1]
+    for n in range(len(big)):
+        if (float(big.real[n]) + 5) < float(small.real[n]):
+            print(big.name[n])
+
+
 ####################### Plot Functions ########################
 
 def check(datas, name_list):
@@ -87,19 +104,48 @@ def gt_plot(datas, names):
     return fig
 
 
-# files = ["evaluation/data/textbook-no_sketch.csv", "evaluation/data/on/off_no_children.csv", "evaluation/data/on/on_no_children.csv", "evaluation/data/on/off_no_children_all_constraints.csv", "evaluation/data/on/off_no_children_constraints.csv"]
+#################### FILES ####################
+
 dir = "evaluation/data/"
+# files = ["evaluation/data/textbook-no_sketch.csv", "evaluation/data/on/off_no_children.csv", "evaluation/data/on/on_no_children.csv", "evaluation/data/on/off_no_children_all_constraints.csv", "evaluation/data/on/off_no_children_constraints.csv"]
 # files = [dir+"st-no_sketch.csv", dir+"st-no_children.csv", dir+"st-no_root.csv"]
 # files = [dir+"st-no_sketch.csv", dir+"st-no_sketch_no_out_ctr.csv", dir+"st-no_sketch_no_out_ctr_new_opt.csv"]
-files = [dir+"st-no_sketch_no_out_ctr_new_opt.csv", dir+"st-sketch_no_children_ctr_new_opt.csv", dir+"st-no_sketch_no_children_ctr_new_opt_flag.csv", dir+"new_no_children_off.csv", dir+"new_no_children_on.csv", dir+"new_no_sketch.csv", dir+"new_no_sketch_both.csv", dir+"new_no_children_on_both.csv", dir+"new_no_children_off_both.csv"]
-out_file = "compare_new_opt_max_min"
+# files = [dir+"st-no_sketch_no_out_ctr_new_opt.csv", dir+"st-sketch_no_children_ctr_new_opt.csv", dir+"st-no_sketch_no_children_ctr_new_opt_flag.csv", dir+"new_no_children_off.csv", dir+"new_no_children_on.csv", dir+"new_no_sketch.csv", dir+"new_no_sketch_both.csv", dir+"new_no_children_on_both.csv", dir+"new_no_children_off_both.csv"]
+files = [dir+'no_sketch.csv', dir+'Off/no_children.csv', dir+'Off/no_root.csv', dir+'On/no_children_on.csv', dir+'On/no_root_on.csv']
 
-csv_list = []
-name_list = []
+out_file = "plots"
+
+# flag_filter = True
+# flag_summarise = True
+
+
+################# PREPARATIONS #################
 
 for file in files:
     csv_list.append(pd.read_csv(file))
     name_list.append(file.rsplit("/", 1)[1][:-4])
+
+
+if flag_filter:
+    files = [dir + 'no_sketch.csv', dir + 'On/no_children_on.csv', dir + 'On/no_root_on.csv', dir + 'On/Filter/no_filter_on.csv', dir + 'On/Filter/only_filter_on.csv']
+    out_file = "filter"
+    new_list = []
+    for data in csv_list:
+        new_list.append(data[data.name.isin(filter)])
+    csv_list = new_list
+
+elif flag_summarise:
+    files = [dir + 'no_sketch.csv', dir + 'On/no_children_on.csv', dir + 'On/no_root_on.csv', dir + 'On/Summarise/no_summarise_on.csv', dir+'On/Summarise/only_summarise_on.csv']
+    out_file = "summarise"
+    new_list = []
+    for data in csv_list:
+        new_list.append(data[data.name.isin(summarise)])
+    csv_list = new_list
+
+
+##################### RUN #####################
+
+# greater_than(csv_list)
 
 sol = check(csv_list, name_list)
 if sol[0]:
--- evaluation/sketch_adding.py
+++ evaluation/sketch_adding.py
@@ -114,13 +114,53 @@ def sketch_no_summarise(sketch, spec):
 
     return sketch_out
 
+def sketch_only_filter(sketch, spec):
+    sketch_out = ''
+    for line in sketch.lines_encoding:
+        root = sketch.lines_encoding[line]
+        if root.real_root != "filter":
+            sketch_out += f'{root.name} = ??\n'
+        else:
+            sketch_out += f'{root.name} = {root.real_root}('
+            for i, child in enumerate(root.children):
+                if i == 0:
+                    sketch_out += f'??, '
+                elif child.real_name:
+                    sketch_out += f'{child.real_name}, '
+            sketch_out = sketch_out[:-2] + ")\n"
+
+    sketch_out += spec['full_sketch'].splitlines()[-1]
+
+    return sketch_out
+
+def sketch_only_summarise(sketch, spec):
+    sketch_out = ''
+    for line in sketch.lines_encoding:
+        root = sketch.lines_encoding[line]
+        if root.real_root != "summarise":
+            sketch_out += f'{root.name} = ??\n'
+        else:
+            sketch_out += f'{root.name} = {root.real_root}('
+            for i, child in enumerate(root.children):
+                if i == 0:
+                    sketch_out += f'??, '
+                elif child.real_name:
+                    sketch_out += f'{child.real_name}, '
+            sketch_out = sketch_out[:-2] + ")\n"
+
+    sketch_out += spec['full_sketch'].splitlines()[-1]
+
+    return sketch_out
+
 # NOTE: Don't forget to copy sketch if we're going to change variables
 
 ######################################################
 
-def parse_sketch(instances):
+def parse_sketch(instances, type_s = None, check = False):
     global last
     all = []
+    summarise = []
+    filter = []
     for file in instances:
         last = file
         with open(file, "r+") as f:
@@ -134,24 +174,51 @@ def parse_sketch(instances):
                 inputs = spec['inputs']
                 sketch.sketch_parser(inputs)
 
-                if not 'sketch_no_children' in spec:
-                    out['sketch_no_children'] = literal(sketch_no_children(sketch, spec))
+                if check:
+                    filter_flag = False
+                    summarise_flag = False
+                    for line in sketch.lines_encoding:
+                        root = sketch.lines_encoding[line]
+                        if root.real_root == "summarise" and not summarise_flag:
+                            summarise.append(file)
+                            summarise_flag = True
+                        elif root.real_root == "filter" and not filter_flag:
+                            filter.append(file)
+                            filter_flag = True
 
-                # Attention when anti_join or summarise since 2 or 3 args
-                if not 'sketch_no_root' in spec:
-                    out['sketch_no_root'] = literal(sketch_no_root(sketch, spec))
+                else:
+                    if not 'sketch_no_children' in spec:
+                        out['sketch_no_children'] = literal(sketch_no_children(sketch, spec))
 
-                if not 'sketch_no_filter' in spec:
-                    out['sketch_no_filter'] = literal(sketch_no_filter(sketch, spec))
+                    # Attention when anti_join or summarise since 2 or 3 args
+                    if not 'sketch_no_root' in spec:
+                        out['sketch_no_root'] = literal(sketch_no_root(sketch, spec))
 
-                if not 'sketch_no_summarise' in spec:
-                    out['sketch_no_summarise'] = literal(sketch_no_summarise(sketch, spec))
+                    if type_s == "filter":
+                        if not 'sketch_no_filter' in spec:
+                            out['sketch_no_filter'] = literal(sketch_no_filter(sketch, spec))
+
+                        if not 'sketch_only_filter' in spec:
+                            out['sketch_only_filter'] = literal(sketch_only_filter(sketch, spec))
+
+                    if type_s == "summarise":
+                        if not 'sketch_no_summarise' in spec:
+                            out['sketch_no_summarise'] = literal(sketch_no_summarise(sketch, spec))
+
+                        if not 'sketch_only_summarise' in spec:
+                            out['sketch_only_summarise'] = literal(sketch_only_summarise(sketch, spec))
 
                 if out != {}:
                     output = yaml.dump(out, default_flow_style=False, sort_keys=False)
                     f.write("\n" + output)
 
-    # print(all)
+    if check:
+        print("All")
+        print(all)
+        print("Filter" + str(len(filter)))
+        print(filter)
+        print("Summarise" + str(len(summarise)))
+        print(summarise)
 
 
 # def remove_sketch(instances):
@@ -227,8 +294,14 @@ def get_comments(instances):
                 #     tb.write(output + "\n")
 
 if __name__ == '__main__':
-    # parse_sketch(['tests-examples/textbook/31.yaml'])
-    # parse_sketch(glob.glob('tests-examples/textbook/*.yaml'))
-    # parse_sketch(glob.glob('tests-examples/scythe/top_rated_posts/*.yaml'))
-    # parse_sketch(glob.glob('tests-examples/scythe/recent_posts/*.yaml'))
-    parse_sketch(glob.glob('tests-examples/spider/architecture/*.yaml'))
+    # parse_sketch(['tests-examples/textbook/35.yaml'])
+    filter = ['tests-examples/textbook/1', 'tests-examples/textbook/10', 'tests-examples/textbook/14', 'tests-examples/textbook/15', 'tests-examples/textbook/16', 'tests-examples/textbook/17', 'tests-examples/textbook/19', 'tests-examples/textbook/2', 'tests-examples/textbook/20', 'tests-examples/textbook/21', 'tests-examples/textbook/22', 'tests-examples/textbook/23', 'tests-examples/textbook/24', 'tests-examples/textbook/25', 'tests-examples/textbook/26', 'tests-examples/textbook/28', 'tests-examples/textbook/29', 'tests-examples/textbook/3', 'tests-examples/textbook/31', 'tests-examples/textbook/35', 'tests-examples/textbook/4', 'tests-examples/textbook/5', 'tests-examples/textbook/6', 'tests-examples/textbook/8', 'tests-examples/textbook/9', 'tests-examples/scythe/top_rated_posts/002', 'tests-examples/scythe/top_rated_posts/013', 'tests-examples/scythe/top_rated_posts/017', 'tests-examples/scythe/top_rated_posts/025', 'tests-examples/scythe/top_rated_posts/031', 'tests-examples/scythe/top_rated_posts/032', 'tests-examples/scythe/top_rated_posts/038', 'tests-examples/scythe/top_rated_posts/043', 'tests-examples/scythe/recent_posts/004', 'tests-examples/scythe/recent_posts/016', 'tests-examples/scythe/recent_posts/019', 'tests-examples/scythe/recent_posts/021', 'tests-examples/scythe/recent_posts/028', 'tests-examples/scythe/recent_posts/031', 'tests-examples/scythe/recent_posts/040', 'tests-examples/scythe/recent_posts/046', 'tests-examples/spider/architecture/0007', 'tests-examples/spider/architecture/0008', 'tests-examples/spider/architecture/0009', 'tests-examples/spider/architecture/0011', 'tests-examples/spider/architecture/0012', 'tests-examples/spider/architecture/0013', 'tests-examples/spider/architecture/0017']
+    summarise = ['tests-examples/textbook/10', 'tests-examples/textbook/14', 'tests-examples/textbook/15', 'tests-examples/textbook/17', 'tests-examples/textbook/18', 'tests-examples/textbook/22', 'tests-examples/textbook/25', 'tests-examples/textbook/3', 'tests-examples/textbook/4', 'tests-examples/textbook/5', 'tests-examples/textbook/6', 'tests-examples/textbook/7', 'tests-examples/textbook/8', 'tests-examples/textbook/9', 'tests-examples/scythe/top_rated_posts/001', 'tests-examples/scythe/top_rated_posts/002', 'tests-examples/scythe/top_rated_posts/004', 'tests-examples/scythe/top_rated_posts/005', 'tests-examples/scythe/top_rated_posts/006', 'tests-examples/scythe/top_rated_posts/007', 'tests-examples/scythe/top_rated_posts/008', 'tests-examples/scythe/top_rated_posts/009', 'tests-examples/scythe/top_rated_posts/011', 'tests-examples/scythe/top_rated_posts/012', 'tests-examples/scythe/top_rated_posts/013', 'tests-examples/scythe/top_rated_posts/014', 'tests-examples/scythe/top_rated_posts/016', 'tests-examples/scythe/top_rated_posts/019', 'tests-examples/scythe/top_rated_posts/021', 'tests-examples/scythe/top_rated_posts/027', 'tests-examples/scythe/top_rated_posts/028', 'tests-examples/scythe/top_rated_posts/029', 'tests-examples/scythe/top_rated_posts/034', 'tests-examples/scythe/top_rated_posts/036', 'tests-examples/scythe/top_rated_posts/037', 'tests-examples/scythe/top_rated_posts/038', 'tests-examples/scythe/top_rated_posts/043', 'tests-examples/scythe/top_rated_posts/047', 'tests-examples/scythe/top_rated_posts/048', 'tests-examples/scythe/top_rated_posts/049', 'tests-examples/scythe/top_rated_posts/051', 'tests-examples/scythe/top_rated_posts/055', 'tests-examples/scythe/top_rated_posts/057', 'tests-examples/scythe/recent_posts/007', 'tests-examples/scythe/recent_posts/009', 'tests-examples/scythe/recent_posts/011', 'tests-examples/scythe/recent_posts/012', 'tests-examples/scythe/recent_posts/016', 'tests-examples/scythe/recent_posts/032', 'tests-examples/scythe/recent_posts/040', 'tests-examples/scythe/recent_posts/045', 'tests-examples/scythe/recent_posts/051', 'tests-examples/spider/architecture/0003', 'tests-examples/spider/architecture/0009', 'tests-examples/spider/architecture/0011']
+    textbook = glob.glob('tests-examples/textbook/*.yaml')
+    top = glob.glob('tests-examples/scythe/top_rated_posts/*.yaml')
+    recent = glob.glob('tests-examples/scythe/recent_posts/*.yaml')
+    spider = glob.glob('tests-examples/spider/architecture/*.yaml')
+
+    parse_sketch(textbook + top + recent + spider, check = True)
+    # parse_sketch(filter, "filter")
+    # parse_sketch(summarise, "summarise")
